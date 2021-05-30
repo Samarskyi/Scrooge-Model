77d6a88b29b91cbcdacebfd88ce111b4c67bec2d
Samarskyi <samgeka@gmail.com>
2021-05-30 08:44:14 +0300

    Added pure balances in total balance entity

--- src/main/kotlin/com/scrooge/model/dto/TotalBalanceDTO.kt
+++ src/main/kotlin/com/scrooge/model/dto/TotalBalanceDTO.kt
@@ -1,9 +1,12 @@
 package com.scrooge.model.dto
 
+import com.binance.api.client.domain.account.AssetBalance
 import java.util.*
 
 open class TotalBalanceDTO(
 		open var userId: String = "",
 		open var date: Date = Date(),
-		open var balances: List<BaseAsset> = listOf()
+		open var updatedAt: Date = Date(),
+		open var balances: List<BaseAsset> = listOf(),
+		open var pureBalances: List<AssetBalance> = listOf()
 )
