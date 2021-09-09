c35003f8365f4d3eb72e466021c186962c404c5c
Samarskyi <samgeka@gmail.com>
2021-09-09 14:00:13 +0300

    Added count into LogEvent

--- src/main/kotlin/com/scrooge/model/entity/LogEvent.kt
+++ src/main/kotlin/com/scrooge/model/entity/LogEvent.kt
@@ -1,8 +1,10 @@
 package com.scrooge.model.entity
 
 import com.scrooge.model.enums.LogType
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.util.*
 
 @Document(collection = "logs")
-data class LogEvent(val userId: String, val type: LogType, val date: Date, val mess: String)
+data class LogEvent(@Id val id: ObjectId = ObjectId.get(), val userId: String, val type: LogType, val date: Date, val mess: String, val count: Long = 0)
