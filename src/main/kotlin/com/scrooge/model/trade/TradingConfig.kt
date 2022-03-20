b4238a815376613db8b2c8138eadff136b052aee
Samarskyi <samgeka@gmail.com>
2022-03-20 09:47:25 +0200

    Clear up trade config

--- src/main/kotlin/com/scrooge/model/trade/TradingConfig.kt
+++ src/main/kotlin/com/scrooge/model/trade/TradingConfig.kt
@@ -5,19 +5,14 @@ package com.scrooge.model.trade
  * @tradeMultiply Represent multiplication factor of the minimum order value
  * @belowAverage Used to determine entry point
  * @fee Per deal. To get full Fee amount need to double multiply
- * @maxCoinBalancePercent Represent max possible open deal value in base currency
  */
 data class TradingConfig(
 		var profit: Double = 3.0,
 		var tradeMultiply: Int = 1,
 		var fee: Double = 0.1,
 		var belowAverage: Double = 1.0,
-		var bigOrderQty: Int = 3,
-		var veryBigOrderQty: Int = 25,
-		var baseCurrencyUp: Double = 3.0,
 		var candleStickIntervalId: String = "1h",
 		var emaPeriod: Int = 3,
-		var maxCoinBalancePercent: Int = 20,
 		val fixedProfit: Boolean = true,
 		val dynamicProfit: Boolean = false,
 		val stopLossProfit: Boolean = false) {
