dc2b0b66f61c9c63a141bf3424b1e3adc693e882
Samarskyi <samgeka@gmail.com>
2023-12-09 13:59:43 +0200

    changed messageToSend dto

--- src/main/kotlin/com/scrooge/model/entity/MessageToSend.kt
+++ src/main/kotlin/com/scrooge/model/entity/MessageToSend.kt
@@ -3,6 +3,7 @@ package com.scrooge.model.entity
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.util.*
 
 @Document(collection = "messages_to_send")
-data class MessageToSend(@Id val id: ObjectId, val userId: ObjectId, val message: String)
+data class MessageToSend(@Id val id: ObjectId, val userId: ObjectId, val message: String, val closeDate: Date? = null)
